f8ae2cd134728e29702a70876416195d43d07f5e
gilad <giladmgm@gmail.com>
2023-01-22 17:17:35 +0200

    add processDirectory

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,4 +1,6 @@
 from CaesarCipher import CaesarCipher
+import json
+import os
 
 
 class VigenereCipher(CaesarCipher):
@@ -26,16 +28,50 @@ def getVigenereFromStr(keyString: str) -> VigenereCipher:
     for i in keyString:
         if i.isalpha():
             if i.islower:
-                keys.append(ord(i)-ord('a'))
+                keys.append(ord(i) - ord('a'))
             else:
-                 keys.append(ord(i)-ord('A'))
+                keys.append(ord(i) - ord('A'))
     return VigenereCipher(keys)
 
 
+def processDirectory(dir_path: str):
+    with open(os.path.join(dir_path, "config.json"), 'r') as f:
+        instructions_dict = json.load(f)
+        for input_file in os.listdir(dir_path):
+            name, extension = os.path.splitext(input_file)
+            if instructions_dict['mode'] == "decrypt":
+                if extension == ".enc":
+                    with open(os.path.join(dir_path, input_file), 'r') as encrypted_file:
+                        encrypted_content = encrypted_file.read()
+                        if instructions_dict['type'] == "Caesar":
+                            a = CaesarCipher(instructions_dict['key'])
+                            decrypted_content = a.decrypt(encrypted_content)
+                        if instructions_dict['type'] == "Vigenere":
+                            if type(instructions_dict['key']) is str:
+                                a = getVigenereFromStr(instructions_dict['key'])
+                            else:
+                                a = VigenereCipher(instructions_dict['key'])
+                            decrypted_content = a.decrypt(encrypted_content)
+                        new_filename = os.path.join(input_file, ".txt")
+                        with open(os.path.join(dir_path, new_filename), 'w') as decrypted_file_result:
+                            decrypted_file_result.write(decrypted_content)
+            else:
+                if extension == ".txt":
+                    with open(os.path.join(dir_path, input_file), 'r') as decrypted_file:
+                        decrypted_content = decrypted_file.read()
+                        if instructions_dict['type'] == "Caesar":
+                            a = CaesarCipher(instructions_dict['key'])
+                            encrypted_content = a.encrypt(decrypted_content)
+                        if instructions_dict['type'] == "Vigenere":
+                            if type(instructions_dict['key']) is str:
+                                a = getVigenereFromStr(instructions_dict['key'])
+                            else:
+                                a = VigenereCipher(instructions_dict['key'])
+                            encrypted_content = a.encrypt(encrypted_content)
+                        new_filename = name + ".enc"
+                        with open(os.path.join(dir_path, new_filename), 'w') as encrypted_file_result:
+                            encrypted_file_result.write(encrypted_content)
+
+
 if __name__ == "__main__":
-    keys = "b?1c d"
-    cipher = getVigenereFromStr(keys)
-    print("abcdefgz  l?!")
-    s = cipher.encrypt("abcdefgz  l?!")
-    print(s)
-    print(cipher.decrypt(s))
+    processDirectory("tests\\test0e")
