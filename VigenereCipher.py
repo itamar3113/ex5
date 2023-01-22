a487d51e0c2199ce724173f9528edef7d8240069
itamar3113 <itamar3113@gmail.com>
2023-01-22 15:00:19 +0200

    decrypt

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -3,17 +3,28 @@ from CaesarCipher import CaesarCipher
 
 class VigenereCipher(CaesarCipher):
     def __init__(self, shifters):
+        super().__init__(shifters[0])
         self.shifters = shifters
-        CaesarCipher.__init__(self, shifters[0])
+        
 
     def encrypt(self, plaintext: str) -> str:
         encrypted = ""
         for i in range(len(plaintext)):
-            self.setShifter(self.shifters[i % len(self.shifters)])
-            encrypted += CaesarCipher.encrypt(self, plaintext[i])
+            super().setShifter(self.shifters[i % len(self.shifters)])
+            encrypted += super().encrypt(plaintext[i])
         return encrypted
+    
+    def decrypt(self, ciphertext: str) -> str:
+        reversedShifters = []
+        for i in self.shifters:
+            reversedShifters.append(-i)
+        decryptor = VigenereCipher(reversedShifters)
+        return decryptor.encrypt(ciphertext)
 
 
 if __name__ == "__main__":
     cipher = VigenereCipher([1, 2, 3])
-    print(cipher.encrypt("abcdefgz  l?!"))
+    print("abcdefgz  l?!")
+    s = cipher.encrypt("abcdefgz  l?!")
+    print(s)
+    print(cipher.decrypt(s))
